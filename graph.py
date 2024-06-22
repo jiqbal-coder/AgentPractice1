05b2b754c5228df5d21f1223025b18fe4c9f225a
jiqbal-coder <125404812+jiqbal-coder@users.noreply.github.com>
2024-06-22 21:11:13 +0000

    First version

---
+++ graph.py
@@ -0,0 +1,31 @@
+from langchain_openai import ChatOpenAI
+from typing import TypedDict, Annotated, List, Dict
+from langgraph.graph import StateGraph, END
+import sqlite3
+from langgraph.checkpoint.sqlite import SqliteSaver
+
+class AgentState(TypedDict):
+    agent: str
+    initialMessage: str
+    responseToUser: str
+    lnode: str
+
+
+class salesCompAgent():
+    def __init__(self, api_key):
+        self.model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=api_key)
+
+        builder = StateGraph(AgentState)
+        builder.add_node("router", self.initial_router)
+        builder.set_entry_point("router")
+        builder.add_edge("router", END)
+        memory = SqliteSaver(conn=sqlite3.connect(":memory:", check_same_thread=False))
+        self.graph = builder.compile(checkpointer = memory)
+
+    def initial_router(self, state: AgentState):
+        print("initial_router")
+        self.responseToUser = "great job"
+        return {
+            "lnode": "initial_router", 
+            "responseToUser": "success"
+        }
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,64 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import os
+from graph import salesCompAgent
+import random
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=st.secrets['LANGCHAIN_API_KEY']
+os.environ["LANGSMITH_API_KEY"]=st.secrets['LANGCHAIN_API_KEY']
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+DEBUGGING=0
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+def start_chat():
+    st.title('Test Zoom Conflict Handler')
+    avatars={"system":"💻🧠","user":"🧑‍💼","assistant":"🎓"}
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    #
+    # Keeping context of conversations.
+    # In practice, this will be say from the Slack - perhaps hash of user-id and channel-id.
+    #
+    if "thread-id" not in st.session_state:
+        st.session_state.thread_id = random.randint(1000, 9999)
+    thread_id = st.session_state.thread_id
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    # Reminder
+    st.sidebar.write("""
+    Use cases:
+    1. Student didnt join.
+    2. Teacher cannot join session.
+    3. Teacher needs to cancel existing session.
+    4. ...
+                      """)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+
+    for message in st.session_state.messages:
+        if message["role"] != "system":
+            avatar=avatars[message["role"]]
+            with st.chat_message(message["role"], avatar=avatar):
+                st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user", avatar=avatars["user"]):
+            st.markdown(prompt)
+        abot=salesCompAgent(st.secrets['OPENAI_API_KEY'])
+        thread={"configurable":{"thread_id":thread_id}}
+        for s in abot.graph.stream({'initialMsg':prompt},thread):
+            if DEBUGGING:
+                print(f"GRAPH RUN: {s}")
+                st.write(s)
+            for k,v in s.items():
+                if DEBUGGING:
+                    print(f"Key: {k}, Value: {v}")
+                if resp := v.get("responseToUser"):
+                    with st.chat_message("assistant", avatar=avatars["assistant"]):
+                        st.write(resp)
+                    st.session_state.messages.append({"role": "assistant", "content": resp})
+
+if __name__ == '__main__':
+    start_chat()
+ 
